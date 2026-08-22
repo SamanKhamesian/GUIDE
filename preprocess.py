452f99c29659b3b16e9e37e77a54a96f86124306
SamanKhamesian <saman.khamesian@gmail.com>
2026-08-21 18:25:16 -0700

    Refactor the project

--- preprocess.py
+++ preprocess.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-from config import PredictorConfig, Threshold, SleepTime, TD3Config
+from config import PredictorConfig, Threshold, SleepTime
 
 
 class Preprocessor:
